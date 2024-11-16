a60ce24b4508c486e45c928a6138a26ba4928441
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2024-11-16 07:25:13 -0500

    first commit

---
+++ src/main/java/com/example/twinguard/TwinguardApplication.java
@@ -0,0 +1,13 @@
+package com.example.twinguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinguardApplication.class, args);
+	}
+
+}
